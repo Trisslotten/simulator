87775fdc1040b7d5bddb46a86a2cfac744ad9c8f
Trisslotten <tristan23@live.se>
2014-12-03 22:45:22 +0100

    fixed center zoom

--- src/simulator/Body.java
+++ src/simulator/Body.java
@@ -1,16 +1,18 @@
+
 package simulator;
 
+import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.GL11;
 
 public class Body {
-
+	
 	public double x, y, xspd, yspd, mass, radius;
 	public double[][] circlePoints;
-
+	
 	public Body(double x, double y) {
 		this(x, y, 0, 0, 1, 10);
 	}
-
+	
 	public Body(double x, double y, double xspd, double yspd, double mass, double radius) {
 		this.x = x;
 		this.y = y;
@@ -21,23 +23,38 @@ public class Body {
 		circlePoints = new double[2][16];
 		setCirclePoints();
 	}
-
+	
 	public void update(double timeScale) {
-		x += xspd*timeScale;
-		y += yspd*timeScale;
+		x += xspd * timeScale;
+		y += yspd * timeScale;
 	}
-
+	
 	public void render(int xoffset, int yoffset, double scale) {
 		double xd = xoffset;
 		double yd = yoffset;
 		GL11.glColor3d(1, 1, 1);
 		GL11.glBegin(GL11.GL_POLYGON);
-		for (int i = 0; i < circlePoints[0].length; i++) {
-			GL11.glVertex2d(circlePoints[0][i] + x - xd, circlePoints[1][i] + y - yd);
+		double displayx = 0;
+		double displayy = 0;
+		double sizelimit = 2;
+		if (circlePoints[0][0] * scale < sizelimit) {
+			double length = circlePoints[0].length;
+			for (int i = 0; i < circlePoints[0].length; i++) {
+				double index = i;
+				double xdraw = scale * (x) + sizelimit * Math.cos(index * 2.0 * Math.PI / length) - xd;
+				double ydraw = scale * (y) + sizelimit * Math.sin(index * 2.0 * Math.PI / length) - yd;
+				GL11.glVertex2d(xdraw, ydraw);
+			}
+		} else {
+			for (int i = 0; i < circlePoints[0].length; i++) {
+				double xdraw = scale * (circlePoints[0][i] + x) - xd;
+				double ydraw = scale * (circlePoints[1][i] + y) - yd;
+				GL11.glVertex2d(xdraw, ydraw);
+			}
 		}
 		GL11.glEnd();
 	}
-
+	
 	public void setCirclePoints() {
 		for (int i = 0; i < circlePoints[0].length; i++) {
 			double length = circlePoints[0].length;
@@ -48,5 +65,5 @@ public class Body {
 			circlePoints[1][i] = ydraw;
 		}
 	}
-
+	
 }
--- src/simulator/Main.java
+++ src/simulator/Main.java
@@ -1,3 +1,4 @@
+
 package simulator;
 
 import static org.lwjgl.opengl.GL11.GL_VERSION;
@@ -6,30 +7,59 @@ import static org.lwjgl.opengl.GL11.glGetString;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 
 public class Main implements Runnable {
-
+	
 	Body b, c;
 	double scale = 1, timeScale = 1;
-
+	double xcam, ycam;
+	
 	protected void init() {
-		b = new Body(300, 400, 0.05, 0, 1, 5);
-		c = new Body(300, 300, -0.05, -0, 1, 5);
+		b = new Body(500, 400, 0.05, 0, 1, 5);
+		c = new Body(400, 320, -0.05, -0, 1, 5);
 	}
-
+	
 	protected void update(double dt) {
-
+		
 		if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
 			timeScale *= 1.0001;
-			System.out.println(timeScale);
+			
 		}
-
-		b.update(timeScale);
-		c.update(timeScale);
-
+		double scalespd = 1.0005;
+		if (Keyboard.isKeyDown(Keyboard.KEY_I)) {
+			double newScale = scale / scalespd;
+			xcam -= width*(1 - newScale / scale)*scale/2;
+			ycam -= height*(1 - newScale / scale)*scale/2;
+			scale = newScale;
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_O)) {
+			double newScale = scale * scalespd;
+			xcam -= width*(1 - newScale / scale)*scale/2;
+			ycam -= height*(1 - newScale / scale)*scale/2;
+			scale = newScale;
+		}
+		
+		if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
+			ycam += dt * 100 / scale;
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_UP)) {
+			ycam -= dt * 100 / scale;
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
+			xcam -= dt * 100 / scale;
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
+			xcam += dt * 100 / scale;
+		}
+		System.out.println();
+		System.out.println(xcam);
+		System.out.println(ycam);
+		// b.update(timeScale);
+		// c.update(timeScale);
 		double dx = b.x - c.x;
 		double dy = b.y - c.y;
 		double distance = Math.sqrt(dx * dx + dy * dy);
@@ -40,14 +70,14 @@ public class Main implements Runnable {
 		b.yspd -= timeScale * Math.sin(angle) * force / b.mass;
 		c.xspd += timeScale * Math.cos(angle) * force / c.mass;
 		c.yspd += timeScale * Math.sin(angle) * force / c.mass;
-
+		
 	}
-
+	
 	protected void render() {
-		b.render(0, 0, scale);
-		c.render(0, 0, scale);
+		b.render((int) xcam, (int) ycam, scale);
+		c.render((int) xcam, (int) ycam, scale);
 	}
-
+	
 	public void run() {
 		if (!(width > 0 && height > 0)) {
 			width = 800;
@@ -55,63 +85,60 @@ public class Main implements Runnable {
 		}
 		glinit(width, height);
 		init();
-
+		
 		double timer = getTime();
 		int updates = 0;
 		double delta = getUpdateDelta();
 		double dt = 0;
 		double lastTime = nanoTime();
 		while (running && !Display.isCloseRequested()) {
-
+			
 			delta = getUpdateDelta();
-			update(0);
+			update(delta);
 			updates++;
 			this.updates++;
-
+			
 			double now = nanoTime();
-
+			
 			dt += (now - lastTime) * 120;
 			lastTime = now;
 			// System.out.println(dt);
 			while (dt >= 1) {
 				GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
 				render();
-				System.out.println("render");
-				System.out.println(dt);
 				dt -= 1.0;
 			}
-
+			
 			if (getTime() - timer > 1) {
-				System.out.println(getMillis());
 				timer += 1;
 				Display.setTitle("updates: " + this.updates + " | ups: " + updates + " | delta: " + delta);
 				updates = 0;
-
+				
 			}
 			Display.update();
 		}
 		Display.destroy();
 	}
-
+	
 	private double nanoTime() {
 		double nanoTime = System.nanoTime();
 		return nanoTime / 1000000000.0;
 	}
-
+	
 	protected int width, height;
-
+	
 	public int updates = 0;
-
+	
 	public static void main(String[] args) {
 		new Main().start();
 	}
-
+	
 	private void glinit(int width, int height) {
 		this.width = width;
 		this.height = height;
 		try {
 			Display.setDisplayMode(new DisplayMode(width, height));
-
+			
 			Display.setTitle("updates: " + updates);
 			Display.create();
 		} catch (LWJGLException e) {
@@ -119,65 +146,65 @@ public class Main implements Runnable {
 		}
 		version = glGetString(GL_VERSION);
 		GL11.glEnable(GL11.GL_TEXTURE_2D);
-
+		
 		GL11.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-
+		
 		// enable alpha blending
 		GL11.glEnable(GL11.GL_BLEND);
 		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-
+		
 		GL11.glViewport(0, 0, width, height);
 		GL11.glLoadIdentity();
 		GL11.glOrtho(0, Display.getWidth(), Display.getHeight(), 0, 1, -1);
 		GL11.glMatrixMode(GL11.GL_MODELVIEW);
-
+		
 	}
-
+	
 	public double getMillis() {
 		return ((double) (Sys.getTime() * 1000)) / (double) (Sys.getTimerResolution());
 	}
-
+	
 	public double getTime() {
 		return ((double) Sys.getTime()) / (double) Sys.getTimerResolution();
 	}
-
+	
 	double oldUpdateTime, newUpdateTime;
-
+	
 	public double getUpdateDelta() {
 		oldUpdateTime = newUpdateTime;
-		newUpdateTime = getTime();
+		newUpdateTime = nanoTime();
 		return (newUpdateTime - oldUpdateTime);
 	}
-
+	
 	public int getWidth() {
 		return width;
 	}
-
+	
 	public void setWidth(int width) {
 		this.width = width;
 	}
-
+	
 	public int getHeight() {
 		return height;
 	}
-
+	
 	public void setHeight(int height) {
 		this.height = height;
 	}
-
+	
 	protected String title = "LWJGL";
 	protected double UPDATES_PER_SECOND = 60.0;
 	protected String version = "";
-
+	
 	protected boolean running = false;
 	private Thread thread;
-
+	
 	public void start() {
 		running = true;
 		thread = new Thread(this, "Display");
 		thread.start();
 	}
-
+	
 	public void stop() {
 		running = false;
 	}
